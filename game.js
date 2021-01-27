296a8459cd571463d194095be907cfc1e57e1940
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2021-01-27 22:39:37 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-
+onload = function () {
+    const unit = 16;
+    const move_step = unit * 5;
+    const fly_step = unit * 8;
+    let platforms_array = [];
+    let moves = [];
+    let pos = 0;
+    let firstTime = true;
+    let onGround = true;
+    const game = new Phaser.Game(320, 240, Phaser.AUTO, 'mynetwork');
+    const game_length = 1850;
+    const game_height = game.height;
+    const ground_height = game_height - 2*unit;
+    const result_font = { font:"20px",fill:"#000",align:"center"};
+    const refresh = document.getElementById('generate-problem');
+    refresh.onclick = function () {
+        game.state.start("GameState");
+    };
