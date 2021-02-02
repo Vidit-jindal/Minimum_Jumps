d3f201f1376c2d59d9789642a5423ab0a94ab349
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2021-02-03 00:03:27 +0530

    Update game.js

--- game.js
+++ game.js
@@ -53,3 +53,8 @@ onload = function () {
             updateState();
         }
     };
+
+game.state.add("BootState",BootState);
+    game.state.add("PreloadState",PreloadState);
+    game.state.add("GameState",GameState);
+    game.state.start("BootState");
