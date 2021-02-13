aa3f7d79985ee1305ea66f031a9f3e38ae42b0fe
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2021-02-13 19:11:59 +0530

    Update game.js

--- game.js
+++ game.js
@@ -161,3 +161,14 @@ function solveProblem(input_arr){
     for(let i=0;i<30;i++){
         res.push([1000,-1]);
     }
+
+    res[0][0] = 0;
+    for(let i=0;i<29;i++){
+        const jump = input_arr[i];
+        for(let j=1;j<=jump && i+j<30;j++){
+            if(res[i+j][0] > res[i][0]+1){
+                res[i+j][0] = res[i][0]+1;
+                res[i+j][1] = j;
+            }
+        }
+    }
